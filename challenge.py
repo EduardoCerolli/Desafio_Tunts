e9d3309a551728febfbc6a2d71443d8cfacc949b
ehsc19 <ehsc19@inf.ufpr.br>
2021-07-26 11:43:01 -0300

    fiz as configurações da api no arquivo challenge.py

--- challenge.py
+++ challenge.py
@@ -5,3 +5,5 @@ import math             # library to use the function ceil
 gc = gspread.service_account (filename = 'service_account.json')        # setting the credentials file
 sh = gc.open_by_url ('https://docs.google.com/spreadsheets/d/1HLNVnAsUohEVQUas9SLWDQuE_N_mZWVg0JqBhbuqAsc/edit#gid=0')      # setting the spreadsheet link
 worksheet = sh.sheet1           # setting the spreadsheet page
+
+# teste
